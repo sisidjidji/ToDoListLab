66659388247e32f2c9377ef77baea1c371368858
sisidjidji <s.azibi@live.fr>
2020-06-22 21:19:56 -0500

    footer and header rendering

--- todolist/src/Componant/Footer/index.js
+++ todolist/src/Componant/Footer/index.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React from "react";
 
-class Footer extends React.Component {
-    render() {
-      return <footer>&copy; 2018 Code Fellows</footer>;
-    }
+
+
+
+function Footer()
+{
+    return(
+     
+     <footer>&copy; 2018 Code Fellows</footer>
+           ) ;
   }
   
   export default Footer;
