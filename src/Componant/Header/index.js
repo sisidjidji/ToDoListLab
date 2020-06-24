85a70555a1ec69027b92d79cb5597b6c839740c3
sisidjidji <s.azibi@live.fr>
2020-06-23 21:09:34 -0500

    d

---
+++ src/Componant/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import'./header.scss'
+
+
+function Header() {
+    return (
+      <header>
+    <h1> Welcome To My To Do List </h1>
+    </header>
+    );
+  }
+  
+  export default Header;
